d1762ed31b3fb97e327d965676ba85fc75bdab59
Brandon Burt <brandxbear@gmail.com>
2021-11-08 05:51:32 -0700

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+
+  import logo from './assets/svelte.png'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <img src={logo} alt="Svelte Logo" />
+  <h3>Ok, this is a little embarrassing for both of us, but maybe it will help if we just pretend I'm a library book and you're about to ...</h3>
+  <h1>check me out!</h1>
+
+  <Counter />
+
+  <p>
+    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
+    apps.
+  </p>
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
+    the officially supported framework, also powered by Vite!
+  </p>
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+    width: 50%;
+  }
+
+  img {
+    height: 16rem;
+    width: 16rem;
+  }
+
+  h3 {
+    font-family: zilla slab;
+    font-size: 2rem;
+    
+  }
+  h1 {
+    font-family: quicksand;
+    color: #ff3e00;
+    font-size: 7rem;
+    font-weight: 100;
+    line-height: 1.1;
+    margin: 2rem auto;
+    max-width: 14rem;
+  }
+
+  p {
+    max-width: 14rem;
+    margin: 1rem auto;
+    line-height: 1.35;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      max-width: none;
+    }
+
+    p {
+      max-width: none;
+    }
+  }
+</style>
---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import App from './App.svelte'
+import '@fontsource/abril-fatface'
+import '@fontsource/bayon'
+import '@fontsource/eb-garamond'
+import '@fontsource/quicksand'
+import '@fontsource/zilla-slab'
+
+const app = new App({
+  target: document.getElementById('app')
+})
+
+export default app
